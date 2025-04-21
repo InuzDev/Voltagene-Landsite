4f87c793175259e0070752827c8fae064c6e6ed2
InuDeveloper <shiibadev@gmail.com>
2025-04-21 18:48:08 -0400

    Eslint error fix in deployment.

--- app/Panel-Installation/page.tsx
+++ app/Panel-Installation/page.tsx
@@ -2,5 +2,9 @@ import React from "react";
 
 export default function PanelInstallation() {
    // Information about our Panel Installation services
-
+   return <>
+      <h1 className="text-center">
+         Under Construction, Stand by
+      </h1>
+   </>
 }
