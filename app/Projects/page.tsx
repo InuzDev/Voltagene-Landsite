b7eac544cb34bb57eed49e7dfbf47fa4dbcc71ea
InuDeveloper <shiibadev@gmail.com>
2025-05-11 21:27:52 -0400

    working on portfolio section

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -7,9 +7,9 @@ export default function ProjectPage() {
       <>
          <main className="min-h-screen items-center text-center">
             <div className="py-96">
-               <h1 className="text-2xl">Under heavy development, expected launch of this page is still an approximate; 5/5/25</h1>
+               <h1 className="text-2xl">Under heavy development, expected launch of this page is still an approximate</h1>
                <br />
-               <h1 className="text-2xl">Bajo desarrollo, se espera un lanzamiento aproximado el 5/5/25</h1>
+               <h1 className="text-2xl">Bajo desarrollo, se espera un lanzamiento aproximado</h1>
             </div>
          </main>
       </>
